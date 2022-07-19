753626c87a4173437775f2c4c2d944f4800d1872
Rafael Melo <rafael@focomultimidia.com>
2022-07-19 14:30:46 -0300

    Movies

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+    production: true,
+    ENDPOINT: 'https://api.themoviedb.org/3',
+    API_KEY: '8c7b911c7d628288f8e3e7663c8da761',
+    ENDPOINT_IMAGE: 'https://image.tmdb.org/t/p/'
 };
